8985d6da0841967449c11ee9ad846261e970a071
Fasina Farook <146752536+fasinafarook@users.noreply.github.com>
2024-11-05 15:03:40 +0530

    Update socket.ts

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000';
+const SOCKET_URL = 'https://tasknestapp.onrender.com';
 let socket: Socket;
 
 export const initSocket = () => {
